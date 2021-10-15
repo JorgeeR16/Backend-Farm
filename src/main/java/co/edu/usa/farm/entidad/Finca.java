e78a244f1cd3c484a9bdfe4b779b882a69dc7198
jorgeramirez2mt <91513363+jorgeramirez2mt@users.noreply.github.com>
2021-10-15 11:45:03 -0500

    la entidad Finca esta funcionando

--- src/main/java/co/edu/usa/farm/entidad/Finca.java
+++ src/main/java/co/edu/usa/farm/entidad/Finca.java
@@ -1,7 +1,55 @@
 package co.edu.usa.farm.entidad;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
+@Entity 
+@Table(name="fincas")
 public class Finca {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private Long extension;
+    private String name;
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Long getExtension() {
+        return extension;
+    }
+    public void setExtension(Long extension) {
+        this.extension = extension;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
     
 }
